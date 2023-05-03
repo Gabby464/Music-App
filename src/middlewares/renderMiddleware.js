6ebae32c9de778ca0e234f12fd6bc96645f4354f
Gabriela P <gabriela.poryazova@gmail.com>
2023-05-03 13:10:04 +0300

    Create page handling middleware

---
+++ src/middlewares/renderMiddleware.js
@@ -0,0 +1,17 @@
+import {render, html} from "./lib.js";
+import { navigationView } from "../views/navigationView.js";
+const header = document.querySelector("#navigation-header");
+const content = document.querySelector('#main-content');
+
+export const renderNavigationMiddleware = (ctx, next) => {
+  // render navigation
+    render(navigationView(ctx), header)
+  next();
+};
+
+export const renderContentMiddleware = (ctx, next) => {
+    ctx.render = (templateResult) => {
+        render(templateResult, content)
+    } 
+    next()
+}
