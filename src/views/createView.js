97159968b0b0728ae0f22b0542163ecd773483f6
Gabriela P <gabriela.poryazova@gmail.com>
2023-05-07 23:26:53 +0300

    Add a new album on the server and on user's side

---
+++ src/views/createView.js
@@ -0,0 +1,54 @@
+import { html } from "../middlewares/lib.js";
+import { createNew } from "../services/catalogService.js";
+
+const createTemplate = (onSubmit) => html `
+        <!--Create Page-->
+        <section class="createPage">
+            <form @submit = ${onSubmit}>
+                <fieldset>
+                    <legend>Add Album</legend>
+
+                    <div class="container">
+                        <label for="name" class="vhide">Album name</label>
+                        <input id="name" name="name" class="name" type="text" placeholder="Album name">
+
+                        <label for="imgUrl" class="vhide">Image Url</label>
+                        <input id="imgUrl" name="imgUrl" class="imgUrl" type="text" placeholder="Image Url">
+
+                        <label for="price" class="vhide">Price</label>
+                        <input id="price" name="price" class="price" type="text" placeholder="Price">
+
+                        <label for="releaseDate" class="vhide">Release date</label>
+                        <input id="releaseDate" name="releaseDate" class="releaseDate" type="text" placeholder="Release date">
+
+                        <label for="artist" class="vhide">Artist</label>
+                        <input id="artist" name="artist" class="artist" type="text" placeholder="Artist">
+
+                        <label for="genre" class="vhide">Genre</label>
+                        <input id="genre" name="genre" class="genre" type="text" placeholder="Genre">
+
+                        <label for="description" class="vhide">Description</label>
+                        <textarea name="description" class="description" placeholder="Description"></textarea>
+
+                        <button class="add-album" type="submit">Add New Album</button>
+                    </div>
+                </fieldset>
+            </form>
+        </section>
+`
+
+export const createView = (ctx) => {
+    const onSubmit = async(e) => {
+        e.preventDefault();
+        const data = Object.fromEntries(new FormData(e.currentTarget));
+        if (Object.values(data).some((value) => !value)) {
+            alert("Please fill out all fields.");
+        }else{
+            await createNew(data);
+            ctx.page.redirect('/')
+        }
+
+    }
+    
+    ctx.render(createTemplate(onSubmit));
+}
